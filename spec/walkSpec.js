e8eefe5239b9d05b5ec915a9e41d77fde386d12a
srikaanthtb <srikaanth.t.b@gmail.com>
2021-02-09 16:45:40 +0000

    test written

---
+++ spec/walkSpec.js
@@ -0,0 +1,7 @@
+describe ('walk', function(){
+
+    it('should say a valid array is true', function(){
+        expect(ten_minute_walk(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])).toEqual(true);
+    });
+
+});
